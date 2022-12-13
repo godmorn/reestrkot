25e59a40aa7d247600d6033c0a2cf29ed254246e
godmorn <115038963+godmorn@users.noreply.github.com>
2022-12-13 12:01:30 +0400

    Add files via upload

---
+++ reestr/script.js
@@ -0,0 +1,163 @@
+var c = 2022;
+var username = prompt("Кошка или Собака номер 1", "Кошка", "Собака");
+console.log(username);
+alert("Вы выбрали " + username);
+var poroda = prompt(
+  "Выберите породу",
+  "Бигль",
+  "Доберман",
+  "Терьер",
+  "Алабай",
+  "Бультерьер"
+);
+console.log(poroda);
+alert("Вы выбрали " + poroda);
+var klichka = prompt("Введите кличку");
+console.log(klichka);
+alert("Кличка вашего питомца " + klichka);
+var god = prompt("Введите год рождения");
+console.log(god);
+d = c - god;
+alert("Полных лет животному " + d);
+console.log(d);
+a1 = [username, poroda, klichka, god, d];
+///
+var username1 = prompt("Кошка или Собака номер 2", "Кошка", "Собака");
+console.log(username1);
+alert("Вы выбрали " + username1);
+var poroda1 = prompt(
+  "Выберите породу",
+  "Бигль",
+  "Доберман",
+  "Терьер",
+  "Алабай",
+  "Бультерьер"
+);
+console.log(poroda1);
+alert("Вы выбрали " + poroda1);
+var klichka1 = prompt("Введите кличку");
+console.log(klichka1);
+alert("Кличка вашего питомца " + klichka1);
+var god1 = prompt("Введите год рождения");
+console.log(god1);
+m = c - god1;
+alert("Полных лет животному " + m);
+console.log(d);
+a2 = [username1, poroda1, klichka1, god1, m];
+///
+var username2 = prompt("Кошка или Собака номер 3", "Кошка", "Собака");
+console.log(username2);
+alert("Вы выбрали " + username2);
+var poroda2 = prompt(
+  "Выберите породу",
+  "Бигль",
+  "Доберман",
+  "Терьер",
+  "Алабай",
+  "Бультерьер"
+);
+console.log(poroda2);
+alert("Вы выбрали " + poroda2);
+var klichka2 = prompt("Введите кличку");
+console.log(klichka2);
+alert("Кличка вашего питомца " + klichka2);
+var god2 = prompt("Введите год рождения");
+console.log(god2);
+l = c - god2;
+alert("Полных лет животному " + l);
+console.log(l);
+a3 = [username2, poroda2, klichka2, god2, l];
+//
+var username3 = prompt("Кошка или Собака номер 4", "Кошка", "Собака");
+console.log(username3);
+alert("Вы выбрали " + username3);
+var poroda3 = prompt(
+  "Выберите породу",
+  "Бигль",
+  "Доберман",
+  "Терьер",
+  "Алабай",
+  "Бультерьер"
+);
+console.log(poroda3);
+alert("Вы выбрали " + poroda3);
+var klichka3 = prompt("Введите кличку");
+console.log(klichka3);
+alert("Кличка вашего питомца " + klichka3);
+var god3 = prompt("Введите год рождения");
+console.log(god3);
+d1 = c - god3;
+alert("Полных лет животному " + d1);
+console.log(d1);
+a4 = [username3, poroda3, klichka3, god3, d1];
+//
+var username4 = prompt("Кошка или Собака номер 5", "Кошка", "Собака");
+console.log(username4);
+alert("Вы выбрали " + username4);
+var poroda4 = prompt(
+  "Выберите породу",
+  "Бигль",
+  "Доберман",
+  "Терьер",
+  "Алабай",
+  "Бультерьер"
+);
+console.log(poroda4);
+alert("Вы выбрали " + poroda4);
+var klichka4 = prompt("Введите кличку");
+console.log(klichka4);
+alert("Кличка вашего питомца " + klichka4);
+var god4 = prompt("Введите год рождения");
+console.log(god4);
+d4 = c - god4;
+alert("Полных лет животному " + d4);
+console.log(d4);
+a5 = [username4, poroda4, klichka4, god4, d4];
+//
+var username5 = prompt("Кошка или Собака номер 6", "Кошка", "Собака");
+console.log(username5);
+alert("Вы выбрали " + username5);
+var poroda5 = prompt(
+  "Выберите породу",
+  "Бигль",
+  "Доберман",
+  "Терьер",
+  "Алабай",
+  "Бультерьер"
+);
+console.log(poroda5);
+alert("Вы выбрали " + poroda5);
+var klichka5 = prompt("Введите кличку");
+console.log(klichka5);
+alert("Кличка вашего питомца " + klichka5);
+var god5 = prompt("Введите год рождения");
+console.log(god5);
+d3 = c - god5;
+alert("Полных лет животному " + d3);
+console.log(d3);
+a6 = [username, poroda5, klichka5, god5, d3];
+//
+var username6 = prompt("Кошка или Собака номер 6", "Кошка", "Собака");
+console.log(username6);
+alert("Вы выбрали " + username6);
+var poroda6 = prompt(
+  "Выберите породу",
+  "Бигль",
+  "Доберман",
+  "Терьер",
+  "Алабай",
+  "Бультерьер"
+);
+console.log(poroda6);
+alert("Вы выбрали " + poroda6);
+var klichka6 = prompt("Введите кличку");
+console.log(klichka6);
+alert("Кличка вашего животного " + klichka6);
+var god6 = prompt("Введите год рождения");
+console.log(god6);
+d5 = c - god6;
+alert("Полных лет животному " + d5);
+console.log(d5);
+a7 = [username6, poroda6, klichka6, god6, d5];
+//
+confirm("Вы желаете продолжить?");
